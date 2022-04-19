2e5e2cd2148ae5bab0a40ede2175dd2ea939760e
HJK9810 <ruby45342@gmail.com>
2022-04-19 15:52:53 +0900

    make main simply

--- code/LotteTicketBox/InputData.java
+++ code/LotteTicketBox/InputData.java
@@ -2,6 +2,7 @@ package LotteTicketBox;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InputData { // for inputs
@@ -68,5 +69,16 @@ public class InputData { // for inputs
 		}
 
 		return input;
+	}
+	
+	protected void ticketingSystem() { // main 에서 돌아갈 메인 시스템 함수
+		CalTickets calc = new CalTickets(); // 주 반복함수가 들어있는 class 선언
+
+		int isExit = 0; // 프로그램 종료를 위한 확인변수
+
+		do { // main 간략화
+			isExit = calc.repeatFunc(); // 반복용 while 함수분리
+			Ticketing.orderList = new ArrayList<>(); // 해당 배열 초기화
+		} while (isExit == 1); // if isExit == 2 => program end
 	}
 }
--- code/LotteTicketBox/Ticketing.java
+++ code/LotteTicketBox/Ticketing.java
@@ -6,13 +6,7 @@ public class Ticketing { // main for ticketing programm
 	protected static ArrayList<OrderData> orderList = new ArrayList<>(); // 티켓발권용 배열
 	
 	public static void main(String[] args) {
-		CalTickets calc = new CalTickets(); // main 반복함수가 들어있는 class 선언
-
-		int isExit = 0; // 프로그램 종료를 위한 확인변수
-
-		do { // main 간략화
-			isExit = calc.repeatFunc(); // 반복용 while 함수분리
-			orderList = new ArrayList<>(); // 해당 배열 초기화
-		} while (isExit == 1); // if isExit == 2 => program end
+		InputData input = new InputData();
+		input.ticketingSystem(); // 주 시스템 함수
 	}
 }
