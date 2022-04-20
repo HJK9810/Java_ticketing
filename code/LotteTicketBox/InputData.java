bb7ad473ad40b2a72681ae71c22f3ba1b8f740d3
HJK9810 <ruby45342@gmail.com>
2022-04-20 13:17:36 +0900

    불필요한 import 상제

--- code/LotteTicketBox/InputData.java
+++ code/LotteTicketBox/InputData.java
@@ -2,7 +2,6 @@ package LotteTicketBox;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InputData { // for inputs
